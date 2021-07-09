81031d7baeb1bdac3abadc081ec26ec8d1786fb3
User-niomic <user@niomic.com>
2021-07-09 23:18:16 +0700

    TUGAS 19 JAVASCRIPT
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas19.js
@@ -0,0 +1,15 @@
+function ulangKata() {
+  var kata1 = "Semangat...";
+  var kata2 = "*****";
+  console.log(kata1.repeat(5));
+  console.log(kata2.repeat(1));
+  console.log(kata2.repeat(5));
+  console.log(kata2.repeat(9));
+  console.log(kata2.repeat(13));
+  console.log(kata2.repeat(19));
+  console.log(kata2.repeat(13));
+  console.log(kata2.repeat(9));
+  console.log(kata2.repeat(5));
+  console.log(kata2.repeat(1));
+}
+ulangKata();
